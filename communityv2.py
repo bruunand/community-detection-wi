aa5ace7160b0dab2ade58d9aa904450a42fd1ee4
Theis <jendal@live.dk>
2018-12-29 21:24:04 +0100

    Added comments

--- communityv2.py
+++ communityv2.py
@@ -8,12 +8,19 @@ class Communities:
         self.friendships, _ = import_data()
 
     def edge_to_remove(self, graph: nx.Graph):
-        betweeness = nx.edge_betweenness_centrality(graph)
-        worst = sorted(betweeness.items(), key=lambda k: k[1], reverse=True)[0][0]
-        return worst
+        # Calculate betweenness of edges.
+        betweenness = nx.edge_betweenness_centrality(graph)
+
+        # Sort by highest betweenness and get the highest elements edge.
+        highest = sorted(betweenness.items(), key=lambda k: k[1], reverse=True)[0][0]
+
+        return highest
 
     def girvan(self):
+        # Get the graph
         graph = self.make_graph(self.friendships)
+
+        # Get status of starting graph
         num_nodes = graph.number_of_nodes()
         c = nx.connected_component_subgraphs(graph)
         l = len(list(c))
@@ -21,37 +28,45 @@ class Communities:
 
         graph_store = [graph.copy()]
 
+        # Continue removing edges until all edges are removed or there are 15 communities.
+        # We stop at 15 as we expect between 2 and 10 communities.
         while l != num_nodes and l < 15:
             graph.remove_edge(*self.edge_to_remove(graph))
             c = nx.connected_component_subgraphs(graph)
             l = len(list(c))
 
+            # If edge removal resulted in more communities, add to store and update prev_l.
             if l > prev_l:
                 graph_store.append(graph.copy())
                 prev_l = l
 
         modularities = []
+
+        # Calculate modularity for all stored splits.
         for index in range(len(graph_store)):
             modularity = self.calculate_modularity(graph_store[index], graph_store[0])
-            modularities.append((modularity, index))
+            modularities.append((modularity, index))        # Store modularity and index.
 
+        # Sort by highest modularity.
         modularities.sort(key=lambda k: k[0], reverse=True)
 
+        # Returns the graph with the best modularity.
         return graph_store[modularities[0][1]]
 
-
-
     def calculate_modularity(self, graph, original_graf):
-        sum = 0
+        _sum = 0
         num_edges = graph.number_of_edges()
+
+        # Calculate modularity by summing percent of edges in a module and percent of edges with one vertice in a module.
         for module in nx.connected_component_subgraphs(graph):
             e = self.edges_in_module(module, original_graf.edges) / num_edges
             a = self.edges_with_end_in_module(module, original_graf.edges) / num_edges
-            sum += e - pow(a, 2)
+            _sum += e - pow(a, 2)
 
-        return sum
+        return _sum
 
-    def edges_in_module(self, graph: nx.Graph, edges):
+    @staticmethod
+    def edges_in_module(graph: nx.Graph, edges):
         nodes = dict(graph.nodes)
         counter = 0
         for v1, v2 in edges:
@@ -60,7 +75,8 @@ class Communities:
 
         return counter
 
-    def edges_with_end_in_module(self, graph: nx.Graph, edges):
+    @staticmethod
+    def edges_with_end_in_module(graph: nx.Graph, edges):
         nodes = dict(graph.nodes)
         counter = 0
         for v1, v2 in edges:
@@ -68,9 +84,6 @@ class Communities:
                 counter += 1
 
         return counter
-
-
-
 
     @staticmethod
     def make_graph(friendships: dict):
@@ -83,6 +96,3 @@ class Communities:
             [graph.add_edge(user, friend) for friend in friends]
 
         return graph
-
-c = Communities()
-print(c.girvan())
