6873812635927140bfcf36dbddbf36cc67109c1c
Theis <jendal@live.dk>
2019-01-15 09:55:08 +0100

    Added accuracy printers for reviews, purchases and clusters

--- data_loader.py
+++ data_loader.py
@@ -24,3 +24,34 @@ def import_data(file_name='data.txt'):
                             reviews[current] = review
 
     return friendships, reviews
+
+
+def import_results(file_name='friendships.reviews.results.txt'):
+    clusters = {}
+    scores = {}
+    purchased = {}
+
+    current = None
+    with io.open(file_name, mode='r', encoding='utf-8') as file:
+        for line in file.readlines():
+            if line.startswith('user'):
+                current = line[6:].strip()
+            else:
+                if current:
+                    if line.startswith('cluster'):
+                        # Strip any newline characters from the cluster
+                        cluster = int(line[9:].strip())
+
+                        clusters[current] = cluster
+                    elif line.startswith('score'):
+                        # Ensures string form of 'x.0', where x is a number between 1 and 5.
+                        score = f'{line[7:].strip()}.0'
+
+                        scores[current] = score
+
+                    elif line.startswith('purchase'):
+                        purchase = line[10:].strip()
+
+                        purchased[current] = purchase
+
+    return clusters, scores, purchased
--- evaluation.py
+++ evaluation.py
@@ -2,12 +2,13 @@ import pickle
 
 from loguru import logger
 
-from data_loader import import_data
-from sentiment import predict, count_vectorizer, preprocessing
+from data_loader import import_data, import_results
+from sentiment import predict, count_vectorizer, preprocessing, class_from_score
 
 
 def calculate_would_buy():
     friendships, reviews = import_data()
+    clusters, scores, purchased = import_results()
 
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -22,20 +23,35 @@ def calculate_would_buy():
 
     logger.debug('Loaded data')
 
-    # Estimate the review label.
+    # Replaces the review with a label.
     for user, review in reviews.items():
         data = preprocessing(review)
-        vector = count_vectorizer(vocab, [data], vocab_index)[0]
+        vector = count_vectorizer(vocab, [data], vocab_index)[0]  # Count vectorizer expects and returns double array.
         reviews[user] = predict(vector, term_freq_matrix, num_terms_pr_class, class_prob)
 
     logger.debug('Predicted reviews')
 
+    would_purchase = calculate_answer(communities, friendships, reviews)
+    logger.debug('Calculated would purchase')
+
+    _print_cluster_yes_percentage(communities, would_purchase)
+    _print_cluster_acccuracy(communities, clusters)
+    _print_review_accuracy(reviews, scores)
+    _print_purchase_accuracy(would_purchase, purchased)
+
+    with open('would_purchase.pkl', 'wb') as f:
+        pickle.dump(would_purchase, f)
+
+
+def calculate_answer(communities, friendships, reviews):
     would_purchase = {}
 
-    # Calculate for each user if they would purchase a fine food.
+    # Calculate for each user if they would purchase a fine food
     for user, community in communities.items():
         count = 0
         score = 0
+
+        # For each friend add the score and respective count
         for friend in friendships[user]:
             friend_community = communities[friend]
 
@@ -53,16 +69,62 @@ def calculate_would_buy():
                 score += label
                 count += 1
 
-        # If there are no friends with review, the answer will be no. Otherwise it is calculated.
+        # If there are no friends with review, the answer will be no. Otherwise it is calculated
         if count == 0:
             logger.debug(user)
             would_purchase[user] = 'no'
         else:
             would_purchase[user] = 'yes' if score / count >= 0.5 else 'no'
 
-    logger.debug('Calculated would purchase')
+    return would_purchase
 
+
+def _print_review_accuracy(our_guesses, dologs_guesses):
+    count = 0
+    skipped = 0
+
+    # Calculates accuracy though we skip reviews with 3 as a score.
+    for user, guess in our_guesses.items():
+        _class = class_from_score(dologs_guesses[user])
+
+        if _class:
+            if guess == _class:
+                count += 1
+        else:
+            skipped += 1
+
+    print(f'Review accuracy: {count / (len(our_guesses) - skipped)}')
+
+
+def _print_purchase_accuracy(our_guesses, dologs_guesses):
+    count = 0
+    for user, guess in our_guesses.items():
+        if guess == dologs_guesses[user]:
+            count += 1
+
+    print(f'Purchase accuracy: {count / len(our_guesses)}')
+
+
+def _print_cluster_acccuracy(our_guesses, dologs_guesses):
+    cluster_conversion_index = {}
+
+    count = 0
+    for user, guess in our_guesses.items():
+        if guess not in cluster_conversion_index:
+            cluster_conversion_index[guess] = dologs_guesses[user]
+
+        guess = cluster_conversion_index[guess]
+
+        if guess == dologs_guesses[user]:
+            count += 1
+
+    print(f'Cluster accuracy: {count / len(our_guesses)}')
+
+
+def _print_cluster_yes_percentage(communities, would_purchase):
     community_yes = {}
+
+    # Calculate the percentage of yes's in each community
     for user, answer in would_purchase.items():
         community = communities[user]
 
@@ -77,10 +139,7 @@ def calculate_would_buy():
     for community, answers in community_yes.items():
         yes_count = answers['yes_count']
         size = answers['size']
-        print(f'{community}: {yes_count/size}')
-
-    with open('would_purchase.pkl', 'wb') as f:
-        pickle.dump(would_purchase, f)
+        print(f'Community {community}: {yes_count/size}')
 
 
 if __name__ == "__main__":
